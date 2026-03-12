74d71b7a0799c6219d5627982a31c2482329ffba
darkstar233 <1606936496@qq.com>
2026-03-12 17:02:52 +0800

    flet测试

--- flettest.py
+++ flettest.py
@@ -2,9 +2,31 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    t = ft.Text(value="Hello, world!", color="green")
-    page.controls.append(t)
-    page.update()
+    massage = ft.Text("趣味数独!", color=ft.Colors.BLUE, size=30)
+    page.add(massage)
+
+    page.add(
+        ft.TextField(
+            label="请输入内容", hint_text="请输入内容"
+        )
+    )
+
+    # 设置窗体标题
+    page.title = "趣味数独"
+
+    # 设置窗体的宽度和高度
+    page.window.width = 500
+    page.window.height = 500
+
+    # 设置窗体的背景颜色
+    # page.bgcolor = ft.Colors.BLUE
+
+    # flet自带的两个显示theme
+    page.theme_mode = ft.ThemeMode.LIGHT
+    # page.theme_mode = ft.ThemeMode.DARK
+
+    # 建议：边框间距padding=0
+    page.padding = 20
 
 
-ft.app(target=main)
+ft.run(main=main)
